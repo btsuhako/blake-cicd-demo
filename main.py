ff853521680e26ffe39b1ae882f390c86bb46a19
Blake Tsuhako <blaket@grow.com>
2017-10-18 00:16:55 -0600

    fix to sum

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def sumMax(args):
     parser.add_argument('integers', metavar='N', type=int, nargs='+',
                         help='an integer for the accumulator')
     parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=numpy.median, default=numpy.max,
+                        const=sum, default=numpy.max,
                         help='sum the integers (default: find the max)')
     parsedArgs = parser.parse_args(args)
     print(parsedArgs.accumulate(parsedArgs.integers))
