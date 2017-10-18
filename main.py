6879fdacab5ea7d02820e8f8d7285fbd09fe1596
Blake Tsuhako <blaket@grow.com>
2017-10-17 23:50:03 -0600

    make main testable

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import argparse
 import sys
+import argparse
 
 
 def sumMax(args):
-
-    print(args.accumulate(args.integers))
-    return args.accumulate(args.integers)
-
-
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('integers', metavar='N', type=int, nargs='+',
                         help='an integer for the accumulator')
     parser.add_argument('--sum', dest='accumulate', action='store_const',
                         const=sum, default=max,
                         help='sum the integers (default: find the max)')
+    parsedArgs = parser.parse_args(args)
+    print(parsedArgs.accumulate(parsedArgs.integers))
+    return parsedArgs.accumulate(parsedArgs.integers)
 
-    args = parser.parse_args()
-    sumMax(args)
+
+if __name__ == "__main__":
+    sumMax(sys.argv[1:])
